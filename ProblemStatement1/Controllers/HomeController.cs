fb00626129bf1958664f95a7d677e3dea762e08b
ram99.masq <ram99.masq@gmail.com>
2020-09-20 17:32:41 +0530

    mocking the UI Display for the output

--- ProblemStatement1/Controllers/HomeController.cs
+++ ProblemStatement1/Controllers/HomeController.cs
@@ -26,46 +26,63 @@ namespace ProblemStatement1.Controllers
         {
             double finalPrice = 0;
             PriceStrategyContext context = null;
+            List<Item> items = new List<Item>();
 
             //<! Inline Block Testing!>
             ////Incase you want to run the code inline and please do uncomment the specific scenario to check the 
             ////Result for different Scenario's
             ////Uncomment Scenarios to test the inline logic for Specific scenarios
 
-            ////Custom Use Case -  All in one Use Case
-            List<Item> items = new List<Item>();
-            items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 3 });
-            items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 2 });
-            items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
-            items.Add(new Item() { ItemID = "D", UnitPrice = 15, OrderItems = 1 });
-            //// finalprice = Total = 205
-
-
             //Scenaria A
-            //items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 1 });
-            //items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 1 });
-            //items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            items = new List<Item>();
+            items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 1 });
+            items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 1 });
+            items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            context = new PriceStrategyContext(items);
+            finalPrice = context.GetCheckoutPrice();
+            ViewBag.ScenariaA = finalPrice;
             //// finalprice = Total = 100
 
 
             //Scenaria B
-            //items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 5 });
-            //items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 5 });
-            //items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            items = new List<Item>();
+            items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 5 });
+            items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 5 });
+            items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            context = new PriceStrategyContext(items);
+            finalPrice = context.GetCheckoutPrice();
+            ViewBag.ScenariaB = finalPrice;
             //// finalprice = Total = 370
 
 
             //Scenaria C
-            //items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 3 });
-            //items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 5 });
-            //items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
-            //items.Add(new Item() { ItemID = "D", UnitPrice = 15, OrderItems = 1 });
-            // finalprice = Total = 280
-
-            //Calling the Business logic for the Price Strategy
+            items = new List<Item>();
+            items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 3 });
+            items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 5 });
+            items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            items.Add(new Item() { ItemID = "D", UnitPrice = 15, OrderItems = 1 });
             context = new PriceStrategyContext(items);
             finalPrice = context.GetCheckoutPrice();
-            return View(finalPrice);
+            ViewBag.ScenariaC = finalPrice;
+            // finalprice = Total = 280
+
+
+            ////Custom Use Case -  All in one Use Case
+            items = new List<Item>();            
+            items.Add(new Item() { ItemID = "A", UnitPrice = 50, OrderItems = 3 });
+            items.Add(new Item() { ItemID = "B", UnitPrice = 30, OrderItems = 2 });
+            items.Add(new Item() { ItemID = "C", UnitPrice = 20, OrderItems = 1 });
+            items.Add(new Item() { ItemID = "D", UnitPrice = 15, OrderItems = 1 });
+            context = new PriceStrategyContext(items);
+            finalPrice = context.GetCheckoutPrice();
+            ViewBag.Customcase = finalPrice;
+            //// finalprice = Total = 205
+          
+
+            ////Calling the Business logic for the Price Strategy
+            //context = new PriceStrategyContext(items);
+            //finalPrice = context.GetCheckoutPrice();
+            return View(ViewBag);
             //<! End - Inline Block Testing!>
         }
 
